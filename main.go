a2ddcb41507ca77d6bac3e51fafd5864e2c6a2d6
PardeshDhakal <pardesh.dhakal1@gmail.com>
2023-10-15 16:30:32 -0400

    figured out how to set up simple http web server

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	w.Write([]byte("<h1 style='color: steelblue'> Hello </h1>"))
+}
+
+func main() {
+	http.HandleFunc("/hello", Hello)
+
+	err := http.ListenAndServe(":5100", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
