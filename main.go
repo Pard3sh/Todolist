3b76ea158b59ca0e6431fd78fd2f4d4de4ade7b6
PardeshDhakal <pardesh.dhakal1@gmail.com>
2023-10-15 16:37:00 -0400

    figuring out how to return json values to front end

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -11,8 +18,22 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1 style='color: steelblue'> Hello </h1>"))
 }
 
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	book := Book{
+		// this is where we would query our database instead of hard coding
+		Title:  "Giovanni's Room",
+		Author: "James Baldwin",
+		Pages:  176,
+	}
+	//makes an encoder with our response writer and then encode our book into that json
+	json.NewEncoder(w).Encode(book)
+}
+
 func main() {
 	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/book", GetBook)
 
 	err := http.ListenAndServe(":5100", nil)
 	if err != nil {
